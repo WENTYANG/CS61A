1a11ab60ce5bb5d5e7c14ca9c804f97015b9a6cc
wentyang <ladadee1031@outlook.com>
2021-03-27 20:07:17 +0800

    Signed-off-by: wentyang <ladadee1031@outlook.com>

---
+++ hw/hw01/curious_if.py
@@ -0,0 +1,12 @@
+def curious(condition,a,b):
+   if condition:
+     return a
+   else:
+     return b
+
+def a():
+   print('a has been executed')
+
+def b():
+   print('b has been executed')
+
---
+++ hw/hw01/hw01-test.py
@@ -0,0 +1,17 @@
+from operator import add, sub
+
+def a_plus_abs_b(a, b):
+    """Return a+abs(b), but without calling abs.
+
+    >>> a_plus_abs_b(2, 3)
+    5
+    >>> a_plus_abs_b(2, -3)
+    5
+    """
+    if b < 0:
+        f = sub
+    else:
+        f = add
+    return f(a,b)
+
+print(a_plus_abs_b(2, 3))
